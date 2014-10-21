873e234df6b43fb843c267c798bd5cfa12fa5366
Maciej Delmanowski <drybjed@gmail.com>
2014-10-21 13:13:04 +0200

    Final fixes to Travis test, hopefully

--- test.sh
+++ test.sh
@@ -5,7 +5,6 @@ cd docs
 # Fix sphinx to ignore non-local image warnings
 filename="$(find /home/travis/virtualenv/ -name environment.py)"
 sed -e '/nonlocal\ image\ URI\ found/ s/^/#/' -i ${filename}
-grep -r "nonlocal image URI found" /home/travis/virtualenv/*
 
 # Test documentation
 sphinx-build -nW -b html -d _build/doctrees . _build/html
