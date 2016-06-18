a5d8313f8037a0cf53333238c08f52df75215f3b
Robin Schneider <ypid@riseup.net>
2016-06-18 14:06:13 +0200

    Don‘t warn about include files not being included in any toctree.
    `includes/*.rst` files can be used to define global and role specific
    hyperlink targets.
    
    Examples:
    
    * https://github.com/ypid/ypid-ansible-common/tree/master/template_role/docs
    * https://github.com/ypid/ansible-x2go_server/tree/master/docs

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,8 @@ release = 'master'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build','debops/*.rst','debops-playbooks/*.rst','ansible/roles/ansible-*/*.rst','ansible/roles/ansible-*/docs/parts']
+# 'includes/*.rst': https://github.com/debops/docs/issues/144
+exclude_patterns = ['_build', 'debops/*.rst', 'debops-playbooks/*.rst', 'ansible/roles/ansible-*/*.rst', 'ansible/roles/ansible-*/docs/parts', 'includes/*.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
