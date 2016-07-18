6b132e7c111919a80199df61e778972aed6d1334
Robin Schneider <ypid@riseup.net>
2016-07-18 21:10:26 +0200

    Remove legacy code.

--- test.sh
+++ test.sh
@@ -7,11 +7,6 @@ cd docs || exit 1
 
 make prepare
 
-# FIXME: Remove. Deprecated by `suppress_warnings`.
-# Fix sphinx to ignore non-local image warnings
-filename="$(find /home/travis/virtualenv/ -name environment.py)"
-sed -e '/nonlocal\ image\ URI\ found/ s/^/#/' -i "${filename}"
-
 # Test documentation
 # -n: nit-picky mode
 sphinx-build -n -b html -d _build/doctrees . _build/html "$@"
